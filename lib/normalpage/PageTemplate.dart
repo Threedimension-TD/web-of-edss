294c5a1531eaecef65945e50eb68d252cd5ff2d5
Threedimension-TD <535061705@qq.com>
2026-04-19 12:48:52 +0800

    Update PageTemplate.dart

--- lib/normalpage/PageTemplate.dart
+++ lib/normalpage/PageTemplate.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:web_of_edss/MyAppBar.dart';
-import 'package:web_of_edss/MyBottomNavigationBar.dart'; // 你的底部导航栏组件
+import 'package:web_of_edss/MyBottomNavigationBar.dart';
 
 class PageTemplate extends StatelessWidget {
-  final Widget bodyContent; // 页面中的主要内容
-  final String backgroundImage; // 背景图路径
-  final double cardWidth; // Card的宽度
+  final Widget bodyContent; 
+  final String backgroundImage; 
+  final double cardWidth; 
   final TextEditingController _controller = TextEditingController();
 
   
   PageTemplate({
     Key? key,
     required this.bodyContent,
-    this.backgroundImage = "assets/images/background.png", // 默认背景
-    this.cardWidth = 2000, // 默认设置 Card 的宽度为 350
+    this.backgroundImage = "assets/images/background.png", 
+    this.cardWidth = 2000, 
   }) : super(key: key);
 
 
 void _createNewPage(BuildContext context) {
-    String pageId = _controller.text.trim(); // 获取输入的 pageId
+    String pageId = _controller.text.trim(); 
     if (pageId.isNotEmpty) {
-      Navigator.pushNamed(context, '/page/$pageId');  // 跳转到动态生成的路由
+      Navigator.pushNamed(context, '/page/$pageId');  
     }
   }
 
@@ -47,13 +47,13 @@ void _createNewPage(BuildContext context) {
               children: [
                 Column(
                   children: [
-                    // 包含 Card 的内容，直接在这里使用 Card
+                    
                     Center(
                       child: Padding(
-                        padding: const EdgeInsets.all(100), // 适当的 padding
+                        padding: const EdgeInsets.all(100), 
                         child: Container(
                           
-                          width: cardWidth, // 固定宽度
+                          width: cardWidth, 
                           child: bodyContent
                         ),
                       ),
@@ -61,7 +61,7 @@ void _createNewPage(BuildContext context) {
                     
                     Container(
                       margin: EdgeInsets.only(top: 50),
-                      child: MyBottomBavigationBar(), // 你的底部导航栏
+                      child: MyBottomBavigationBar(),
                     ),
                   ],
                 ),
